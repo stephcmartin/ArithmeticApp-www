6c7c01ffaeb9d764492139eea151ff6546707cf8
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-03 18:30:18 -0400

    Added nav bar to app

--- src/components/app.js
+++ src/components/app.js
@@ -5,18 +5,33 @@ import QuestionByID from './questionByID'
 import Welcome from './welcome.js'
 import Footer from './footer.js'
 import { Route, Switch } from 'react-router-dom'
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
+ <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Nav className="navbar-nav mr-auto">
+          <IndexLinkContainer to="/">
+            <NavItem className="nav-link">Dashboard</NavItem>
+          </IndexLinkContainer>
+          <LinkContainer to="/questions">
+            <NavItem className="nav-link">List of questions</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/questions/new">
+            <NavItem className="nav-link">Create new question</NavItem>
+          </LinkContainer>
+        </Nav>
+        </Navbar>
         <Welcome />
-        <Switch>
-            <Route path="/questions/new" component={CreateQuestion} />
-            <Route path="/questions" component={Questions} />
-            <Route path="/questions/:questionId" component ={QuestionByID} />
-        </Switch>
+          <Switch>
+              <Route path="/questions/new" component={CreateQuestion} />
+              <Route path="/questions" component={Questions} />
+              <Route path="/questions/:questionId" component ={QuestionByID} />
+          </Switch>
         <Footer />
       </div>
     );

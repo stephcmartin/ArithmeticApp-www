1ec7a6ce17cbd1e2b235ac9197a54278e9beab5b
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-03 18:29:41 -0400

    Edited Styling on questions page

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,7 +3,14 @@ import { fetchQuestions } from '../actions/index';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import SearchBar from './searchBar.js'
 import _ from 'lodash';
+
+const listOfQuestions ={
+ overflowY: 'scroll',
+ height:'400px',
+}
+
 
 export class QuestionSummary extends Component {
   constructor(props){
@@ -21,24 +28,37 @@ export class QuestionSummary extends Component {
 
     const renderQuestions = _.map(this.props.questions.questions, question => {
        return (
-       <div className="col-md-8 list-group">
-        <Link to ={"/questions/" + question._id}>
-        <li key={question._id}> Question: {question.question} </li>
-        <button className="btn btn-primary">View</button>
-        <button className="btn btn-danger">Delete</button>
-        </Link>
+         <div className="row">
+          <div className="col-md-8">  
+            <Link to ={"/questions/" + question._id}>
+            <li key={question._id}> 
+            Question: {question.question} <br />
+            Answer: {question.answer} <br />
+            Distractors: {question.distractors}
+            </li>
+            </Link>
+          </div>
+          <div className="col-md-4">
+          <button className="btn btn-primary"
+          onClick={() => 
+          {this.props.history.push(`/questions/${question._id}`)}}>View</button>
+          <button className="btn btn-danger">Delete</button>
+          </div>
       </div>
        )
        
       }
     )
     return(
-
-      <div>
-        <button className="createNew btn btn-primary" onClick={() => {this.props.history.push('/questions/new')}}> Create New </button>
+        <div>
+          <SearchBar />
+      <div className="listOfQuestions" style={listOfQuestions}>
+        
+       
         <ul>
         {renderQuestions}
         </ul>
+      </div>
       </div>
     )
   }

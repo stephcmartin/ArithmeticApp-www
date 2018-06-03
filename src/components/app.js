c5f57d2e605d9402a453f0b06d8f22fa1e5da8fb
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-03 12:26:25 -0400

    Added Route to question by ID

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Questions from './Questions'
 import CreateQuestion from './createQuestion'
+import QuestionByID from './questionByID'
 import Welcome from './welcome.js'
 import Footer from './footer.js'
 import { Route, Switch } from 'react-router-dom'
@@ -12,10 +13,9 @@ export default class App extends Component {
       <div>
         <Welcome />
         <Switch>
-        
             <Route path="/questions/new" component={CreateQuestion} />
             <Route path="/questions" component={Questions} />
-        
+            <Route path="/questions/:questionId" component ={QuestionByID} />
         </Switch>
         <Footer />
       </div>

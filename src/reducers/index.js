c8324430d46f3605e408ac21d334e00101c88c59
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-02 20:09:45 -0400

    Refactored reducers to have questionID with data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import QuestionsReducer from './reducer_questions'
 
 const rootReducer = combineReducers({
-  // state: (state = {}) => state
   questions : QuestionsReducer
 });
 
--- src/reducers/reducer_questions.js
+++ src/reducers/reducer_questions.js
@@ -1,7 +1,11 @@
-import { FETCH_QUESTIONS_PENDING, FETCH_QUESTIONS_ERROR, FETCH_QUESTIONS_SUCCESS} from "../actions/";
+import _ from 'lodash'
+import { FETCH_QUESTIONS_PENDING, FETCH_QUESTIONS_ERROR, FETCH_QUESTIONS_SUCCESS,
+  POST_QUESTION_PENDING, POST_QUESTION_ERROR, POST_QUESTION_SUCCESS
+} from "../actions/";
+
 
 const initialState = {
-  questions: [],
+  questions: {},
   success: false,
   message: "No Data Loaded",
   loading: true
@@ -9,6 +13,7 @@ const initialState = {
 
 export default function(state = initialState, action){
   switch(action.type){
+    // Fetching Questions
     case FETCH_QUESTIONS_PENDING:
     return {
       questions: action.questions,
@@ -17,20 +22,42 @@ export default function(state = initialState, action){
     case FETCH_QUESTIONS_SUCCESS:
     return {
             ...state,
-            questions: action.questions,
+            questions: _.mapKeys(action.questions, '_id'),
             success: action.success,
             loading: false,
             message: action.message
-  };
-  case FETCH_QUESTIONS_ERROR:
-  return {
+      };
+      case FETCH_QUESTIONS_ERROR:
+      return {
+              ...state,
+              questions: action.questions,
+              success: action.success,
+              loading: true,
+              message: action.message
+    };
+    // Posting Questions
+    case POST_QUESTION_PENDING:
+    return {
+      questions: action.question,
+      loading: true
+    };
+    case POST_QUESTION_SUCCESS:
+    return {
+            ...state,
+            questions: {...state.questions, [action.question._id]:action.question },
+            success: action.success,
+            loading: false,
+            message: action.message
+    };
+    case POST_QUESTION_ERROR:
+    return {
           ...state,
-          questions: action.questions,
+          questions: action.question,
           success: action.success,
           loading: true,
           message: action.message
-};
-  default:
-      return state;
-  }
+    };
+    default:
+        return state;
+    }
 }

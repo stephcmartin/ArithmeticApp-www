f56010ff70ab9d4586b5b8e1371c15b58b045672
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-03 20:42:47 -0400

    added question by ID reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import QuestionsReducer from './reducer_questions'
+import QuestionByIdReducer from './reducer_question_by_id'
 
 const rootReducer = combineReducers({
-  questions : QuestionsReducer
+  questions : QuestionsReducer,
+  question: QuestionByIdReducer
 });
 
 export default rootReducer;

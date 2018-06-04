80db117f58ef6b241a2dc94176769a4fcf041319
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-04 15:30:02 -0400

    added console logs to test onClick

--- src/components/questionByID.js
+++ src/components/questionByID.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { fetchQuestionById } from '../actions/index';
+import { fetchQuestionById , deleteQuestionById } from '../actions/index';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
@@ -8,6 +8,13 @@ export class QuestionById extends React.Component {
 
     constructor(props){
         super(props)
+        this.deleteClickHandeler = this.deleteClickHandeler.bind(this);  
+      }
+
+      deleteClickHandeler(e, foo) {
+        e.preventDefault()
+        console.log('deleting this question', foo)
+        this.props.deleteQuestionById(foo)
       }
 
       componentDidMount(){
@@ -16,6 +23,8 @@ export class QuestionById extends React.Component {
 
 
     render(){
+      const foo = this.props.question.question._id
+      console.log('foo', foo)
        return (
          <div className="questionById">
            <h4>Question:</h4>
@@ -24,7 +33,7 @@ export class QuestionById extends React.Component {
            <p>{this.props.question.question.answer}</p>
            <h4>Distractor:</h4>
            <p>{this.props.question.question.distractors}</p>
-           <button className="btn btn-danger" onClick={()=> {console.log('delete this item', this.props.question.question._id)}}>Delete</button>
+           <button type="submit" className="btn btn-danger" onClick={(e, foo) => {this.deleteClickHandeler(e, foo)}}>Delete</button>
           <Link to ={"/questions"}><p><em><br />Back to list of questions</em></p></Link>
            </div>
        )

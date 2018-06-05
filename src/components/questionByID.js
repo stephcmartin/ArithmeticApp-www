21b812fce12abf2841452708a990a5ef6cf38cb7
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-04 20:52:37 -0400

    Added basic logic to render editing and save functionality.

--- src/components/questionByID.js
+++ src/components/questionByID.js
@@ -9,12 +9,47 @@ export class QuestionById extends React.Component {
 
     constructor(props){
         super(props)
+        this.state = {
+          isEditing: false,
+          question: this.props.question.question.question,
+          answer: this.props.question.question.answer,
+          distractors: this.props.question.question.distractors
+        };
+
+        this.saveEdit = this.saveEdit.bind(this);
+
+        this.editClickHandeler = this.editClickHandeler.bind(this);
         this.deleteClickHandeler = this.deleteClickHandeler.bind(this);
+      }
+
+
+      saveEdit() {
+        // This will save the data and
+        // send a POST action with the editQuestionById() action + reducer
+        console.log('saving updates')
       }
 
       deleteClickHandeler() {
         this.props.deleteQuestionById(this.props.question.question._id)
       }
+
+      editClickHandeler(){
+        const foo = this.props.question.question
+        this.setState({isEditing: !this.state.isEditing})
+
+      }
+
+      handleSubmit(event) {
+          alert('Oops! Looks like you found a bug. Sit back and have a coffee while we work on this.');
+          event.preventDefault();
+          }
+
+      onInputChange(name, value){
+        let newState={}
+        newState[name] = value
+        this.setState(newState)
+      }
+
 
       componentDidMount(){
         this.props.fetchQuestionById(this.props.match.params.id)
@@ -22,6 +57,44 @@ export class QuestionById extends React.Component {
 
 
     render(){
+      if (this.state.isEditing) {
+          return (
+          <div className="editingForm">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              <h4>Question:</h4>
+              <input
+              value={this.props.question.question.question}
+              type="text"
+              name="question"
+              onChange={e => this.onInputChange('question', e.target.value)}
+              />
+            </label>
+            <label>
+              <h4>Answer:</h4>
+              <input
+              value={this.props.question.question.answer}
+              type="text"
+              name="answer"
+              onChange={e => this.onInputChange('answer', e.target.value)}
+            />
+            </label>
+            <label>
+              <h4>Distractors:</h4>
+              <input
+              value={this.props.question.question.distractors}
+              type="text"
+              name="distractors"
+              onChange={e => this.onInputChange('distractors', e.target.value)}
+              />
+            </label>
+            <br />
+              <input type="submit" className="btn btn-info"/>
+          </form>
+          <Link to ={"/questions"}><p><em><br />Back to list of questions</em></p></Link>
+          </div>
+          )
+        }
        return (
          <div className="questionById">
            <h4>Question:</h4>
@@ -30,9 +103,15 @@ export class QuestionById extends React.Component {
            <p>{this.props.question.question.answer}</p>
            <h4>Distractor:</h4>
            <p>{this.props.question.question.distractors}</p>
+
+           <button className="btn btn-info"
+           onClick={(e) => {this.editClickHandeler(this)}}>Edit</button>
+           <br />
+
            <button type="submit" className="btn btn-danger"
            onClick={(e) => {this.deleteClickHandeler(this)}}>
            Delete</button>
+
           <Link to ={"/questions"}><p><em><br />Back to list of questions</em></p></Link>
            </div>
        )
